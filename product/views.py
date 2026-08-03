fc19aaa64d066f42dafe094653578a6cff1e006d
Nicolly <j4kwzx@gmail.com>
2026-08-03 12:53:53 -0300

    Corrigindo permissões: Product publico, Order requer autenticação

--- product/views.py
+++ product/views.py
@@ -1,5 +1,4 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -8,4 +7,3 @@ from .serializers import ProductSerializer
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [IsAuthenticated]
